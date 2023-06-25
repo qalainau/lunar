ab391dbef0d27f6be025752e0c535dbe0771bba4
qalainau <qalainau@gmail.com>
2023-06-25 23:32:59 +0900

    fix

--- packages/admin/routes/includes/deliveries.php
+++ packages/admin/routes/includes/deliveries.php
@@ -2,14 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use Lunar\Hub\Http\Livewire\Components\Deliveries\DeliveriesIndex;
-use Lunar\Hub\Http\Livewire\Components\Deliveries\DeliveriesOrderLine;
+
+//use Lunar\Hub\Http\Livewire\Components\Deliveries\DeliveriesOrderLine;
 
 Route::group([
     'middleware' => 'can:deliveries:deliveries',
     // 'middleware' => 'can:settings:manage-staff',
 ], function () {
     Route::get('/', DeliveriesIndex::class)->name('hub.deliveries.index');
-    Route::get('/order-line/{brand_id}/{filter}', DeliveriesOrderLine::class)->name('hub.deliveries.order-line');
+    //Route::get('/order-line/{brand_id}/{filter}', DeliveriesOrderLine::class)->name('hub.deliveries.order-line');
     // Route::get('{id}', ReportShow::class)->withTrashed()->name('hub.reports.show');
 });
 
